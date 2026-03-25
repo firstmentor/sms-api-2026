9a3a797abc543ba36fa24bd71b91a0925efd4786
firstmentore <jainvikas887@gmail.com>
2026-03-25 12:43:27 +0530

    sdd

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.cookies.token;
-    // console.log(token)
+    //console.log(token)
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
     }
